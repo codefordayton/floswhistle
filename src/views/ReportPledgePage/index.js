5d42658f0362ce534ed5d3cae748a9c467aba8ff
Michael Bowman <bowmanmc@gmail.com>
2018-04-22 18:27:33 -0400

    add react-select

--- src/views/ReportPledgePage/index.js
+++ src/views/ReportPledgePage/index.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Select from 'react-select';
 
 
-const ReportPledgePage = props => {
-	return (
-		<div className="ReportPledgePage">
+class ReportPledgePage extends React.Component {
 
-			<p>
-				This is a pledge-common practice online in
-				the medical field. It's a form where the user
-				swears that they are a currently working
-				professional caregiver and were on duty at the
-				facility at the time of the incident they are
-				about to report.
-			</p>
+	constructor(props, context) {
+		super(props, context);
 
-			<div className="agreement-button">
-				<Link className="button" to="/report">I Agree</Link>
+		this.state = {
+			TypeOptions: [
+				{ value: 'RN', label: 'RN' },
+				{ value: 'LPN', label: 'LPN' },
+				{ value: 'CPCT/CNA', label: 'CPCT/CNA'}
+			],
+			typeSelection: 'RN'
+		};
+
+		this.onTypeChange = this.onTypeChange.bind(this);
+	}
+
+	onTypeChange(newType) {
+		this.setState({typeSelection: newType});
+		console.log('NewType: ' + newType);
+	}
+
+	render() {
+		return (
+			<div className="ReportPledgePage">
+				<label>I am a: </label>
+				<Select
+					onChange={this.onTypeChange}
+					options={this.state.TypeOptions}
+					simpleValue
+					clearable={false}
+					searchable={false}
+					value={this.state.typeSelection}
+				/>
+				<p>
+					This is a pledge-common practice online in
+					the medical field. It's a form where the user
+					swears that they are a currently working
+					professional caregiver and were on duty at the
+					facility at the time of the incident they are
+					about to report.
+				</p>
+
+				<div className="agreement-button">
+					<Link className="button" to="/report">I Agree</Link>
+				</div>
+
 			</div>
-
-		</div>
-	);
+		);
+	}
 };
 
 export default ReportPledgePage;
