11070a45c1670fc87bb4c826de01034fab9600cc
Michael Bowman <bowmanmc@gmail.com>
2018-04-22 19:59:57 -0400

    updated front page text

--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -13,12 +13,36 @@ const HomePage = props => {
 
 			<div className="content">
 				<p>
-					A short statement that explains why
-					Flo's Whistle was created, consectetur,
-					adipisci velit, sed quia non numquam
-					eius modi tempora incidunt ut labore et
-					dolore magnam aliquam quaerat
-					voluptatem.
+				Flo’s Whistle is a secure, anonymous platform where direct
+				care nurses can log instances  of compromised patient safety
+				due to inadequate staffing.  Reports will be aggregated and
+				shown on a US map. This year-long, prospective, experimental
+				data-collection project will help us all better understand
+				the magnitude of the inadequate nurse staffing problem.
+				</p>
+
+				<p>
+				We believe that professional nursing care providers who are
+				at the bedside, providing direct care, are far more accurate
+				judges of patients’ needs than any administrator or consultant
+				with spreadsheets and algorithms.
+				</p>
+
+				<p>
+				America’s nurses are highly educated and rigorously trained
+				to give excellent care, but when we’re deprived of the time
+				and resources required to deliver that care, too often we
+				fail our patients. This is unacceptable.
+				</p>
+
+				<p>
+				With Flo’s Whistle, we’re following the example of nurse
+				Florence Nightingale, a pioneer in the use of statistics and
+				visual display of data. Her research revealed patterns in
+				causes of death among soldiers in the Crimean war, lighting
+				the way for rational policy reforms, and dramatically
+				reducing the suffering and loss of life.
+				We hope to do the same.
 				</p>
 			</div>
 
